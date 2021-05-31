57b35f8dceeba52b71ac0eadc917ce3f12861469
Ethan <esmoltz@gmail.com>
2021-05-31 16:46:52 -0400

    forked

---
+++ C solution using Semaphores/cmake-build-debug/CMakeFiles/C_solution_using_Semaphores.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_solution_using_Semaphores.dir/SantaClaus.c.o"
+  "C_solution_using_Semaphores"
+  "C_solution_using_Semaphores.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_solution_using_Semaphores.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
